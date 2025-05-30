f1f1485510d95dcd47de9b0f12f183b8d611b25f
lewuiis93 <lojaepesca@gmail.com>
2025-05-31 00:57:27 +0100

    Corrige chamada login para versão antiga da lib

--- app.py
+++ app.py
@@ -23,9 +23,7 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=1
 )
 
-name, authentication_status, username = authenticator.login(
-    form_name="Login", location="main"
-)
+name, authentication_status, username = authenticator.login("Login", "main")
 
 if authentication_status is False:
     st.error("Usuário ou senha incorretos.")
